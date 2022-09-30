a6985c5a0813cc6cf6d52583a3716a89bdbc14e8
Tomasgt25 <113174906+Tomasgt25@users.noreply.github.com>
2022-09-30 20:07:30 +0200

    Add files via upload

---
+++ Ejercicio6/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float base1,altura1,base2,altura2;
+        System.out.println("Introduzca la base del primer rectángulo:");
+        base1=sc.nextFloat();
+        System.out.println("Introduzca la altura del primer rectángulo:");
+        altura1=sc.nextFloat();
+        System.out.println("Introduzca la base del segundo rectángulo:");
+        base2=sc.nextFloat();
+        System.out.println("Introduzca la altura del segundo rectángulo:");
+        altura2=sc.nextFloat();
+
+        float area1 = base1*altura1;
+        float area2 = base2*altura2;
+        if (area1>area2) {
+            System.out.println("El rectángulo 1 tiene mayor área");
+        }
+         else if (area1 == area2) {
+            System.out.println("Los dos rectángulos tienen el mismo área");
+        }
+         else {
+            System.out.println("El rectángulo 2 tiene mayor área");
+        }
+    }
+}
