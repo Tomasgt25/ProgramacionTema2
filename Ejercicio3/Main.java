8fd582b2750a5380036af183df69c95e8e3dd5af
Tomasgt25 <113174906+Tomasgt25@users.noreply.github.com>
2022-09-30 18:47:23 +0200

    Add files via upload

---
+++ Ejercicio3/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double radio;
+        double perimetro;
+
+        System.out.println("Indique el radio");
+        radio=sc .nextDouble();
+        double area = Math.pow(radio,2)*Math.PI;
+        perimetro = 2*Math.PI*radio;
+        System.out.println("El área es " + ((double) Math.round(area*100d) /100d));
+        System.out.println("El perimetro es " + (double) Math.round(perimetro*100d) /100d);
+    }
+}
