5a92c02b7065383ab8f9245df04369d72bef7812
Tomasgt25 <tomasgt1996@gmail.com>
2022-10-07 15:42:37 +0200

    Ejercicios 1-15

--- Ejercicio12/src/Ejercicio12.java
+++ Ejercicio12/src/Ejercicio12.java
@@ -1,5 +1,24 @@
-public class Main {
+package Ejercicio12;
+
+import java.util.Scanner;
+
+/* Programa que me pida 5 números y me diga cual es
+el mayor usando "for"
+ */
+public class Ejercicio12 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc=new Scanner(System.in);
+        int mayor=0, num;
+
+        for(int i=0; i<=4; i=i+1){
+            System.out.println("Introduzca un número");
+            num= sc.nextInt();
+            mayor=num;
+            if (num>mayor);{
+
+            }
+        }
+        System.out.println("El número mayor es: " + mayor);
+
     }
 }
--- Ejercicio13/src/Ejercicio13.java
+++ Ejercicio13/src/Ejercicio13.java
@@ -1,5 +1,18 @@
-public class Main {
+package Ejercicio13;
+/* Programa que calcule la suma de los 100 primeros números*/
+
+public class Ejercicio13 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int suma=0; //ACUMULADOR
+        for (int i= 0; i<=100; i=i+1){
+            suma+=1;
+
+            System.out.println("La suma de los 100 primeros números es: " + i);
+       /* Esto también se haría con la fórmula de Gauss:
+       System.print.outln ((n*(n+1)/2));
+       (n*(n+1)/2)
+        */
+
+        }
     }
 }
